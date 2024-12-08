0737ae77103850116cd2eea94a18e0f0a0f24298
pavan-ki <pavan.ki@hotmail.com>
2024-12-08 08:44:32 +0000

    updates

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function populateMenu() {
         "Chapter 03 - FIRE HAZARDS AND FIRE PREVENTION": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
         "Chapter 04 - ERSTWHILE TARIFF – RULES AND RATING": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
         "Chapter 05 - DOCUMENTS": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
-        "Chapter 06 - IT APPLICATIONS IN MOTOR INSURANCE": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
+        "Chapter 06 - UNDERWRITING": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
         "Chapter 07 - CONSUMER DELIGHT": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
         "Chapter 08 - THIRD PARTY LIABILITY INSURANCE": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
         "Chapter 09 - PROCEDURES FOR FILING AND DEFENDING": ["Easy.json", "Medium.json", "Hard.json", "Extreme.json", "Old.json", "Summary.json"],
